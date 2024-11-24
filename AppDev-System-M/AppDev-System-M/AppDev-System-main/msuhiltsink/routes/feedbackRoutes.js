b872fdbf85af23f10df89b203f9fc7c8e317ae71
Alfia A. Bahia <161782682+Bahia2004@users.noreply.github.com>
2024-11-24 09:07:41 +0800

    Delete AppDev-System-M/AppDev-System-M/AppDev-System-main directory

--- AppDev-System-M/AppDev-System-M/AppDev-System-main/msuhiltsink/routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-/*const express = require('express');
-const router = express.Router();
-const feedbackController = require('../controllers/feedbackController');
-
-router.get('/:patient_id', feedbackController.getFeedback);
-router.post('/add', feedbackController.addFeedback);
-
-module.exports = router;
