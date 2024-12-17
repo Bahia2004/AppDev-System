ddb8ea52f493b0c385c8dec5e62b15a002bbf9de
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-12-17 21:35:37 +0800

    Update mailer.js

--- msuhiltsink/utils/mailer.js
+++ msuhiltsink/utils/mailer.js
@@ -4,15 +4,15 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail', // You can change this to any email provider
     auth: {
-        user: 'aash100227@gmail.com',  // Your email BTW ARI AYY EMAIL KO
-        pass: 'ewir gyae wofs hali'    // Your password or an app-specific password ETO NMN AYY NAKUHA KO SA 2 STEP VERIFICATION MAIKLI LNG PROSESO KUNG GUSTO NYO MAG TRY TURUAN KO KAU KUNG SAN PEDE KUNIN
+        user: '',  // Your email BTW ARI AYY EMAIL KO
+        pass: ''    // Your password or an app-specific password ETO NMN AYY NAKUHA KO SA 2 STEP VERIFICATION MAIKLI LNG PROSESO KUNG GUSTO NYO MAG TRY TURUAN KO KAU KUNG SAN PEDE KUNIN
     }
 });
 
 // Function to send an email
 async function sendEmail(to, subject, text) {
     const mailOptions = {
-        from: 'aash100227@gmail.com',  // Your email
+        from: '',  // Your email
         to: to,  
         subject: subject, 
         text: text  
